36c4f28ceac931fb284be0169df348571d22a18c
EduardoIsaacC <160290260+EduardoIsaacC@users.noreply.github.com>
2024-03-05 01:48:49 +0000

    Version 5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <string>
 using namespace std;
 
@@ -7,20 +8,37 @@ using namespace std;
 #include <Dibujo.hpp>
 int main(int argc, char const *argv[])
 {
-    Dibujo dibujo("./assets/donperro.txt");
-    dibujo.Dibujar();
+    list<Dibujo>dibujos;
+    
+    Dibujo dibujo1("./assets/donperro.txt");
+    dibujo1.Dibujar();
+    Dibujo dibujo2("./assets/cesarin.txt");
+    dibujo2.Dibujar();
+
+    //dibujos.push_front(dibujo);
+    //dibujos.push_front(dibujo2);
 
     /* code */
     Mascota m1;
     Mascota m2;
     Mascota m3;
 
+    list<Mascota>mascotas;
+    mascotas.push_back(m1);
+    mascotas.push_back(m2);
+    mascotas.push_back(m3);
+
     Alimento alimento(5);
 
-    m1.Comer(alimento);
-    m2.Comer(alimento);
-    m3.Comer(alimento);
+    for (auto &&mascotaActual : mascotas)
+    {
+        mascotaActual.Comer(alimento);
 
-    cout<<"Mascota "<<m1.LeerEnergia() <<endl;
+    }
+    for (auto &&mascotaActual : mascotas)
+    {
+        cout<<"Mascota "<<mascotaActual.LeerEnergia() <<endl;
+    }
+    
     return 0;
 }
